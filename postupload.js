73f98b207e7c18fe1adbe211f5f5d128a24ff119
usageek1266 <usageek1266@gmail.com>
2021-01-26 22:28:47 +0800

    add Content-Type for upload

--- postupload.js
+++ postupload.js
@@ -2,6 +2,8 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const FormData = require('form-data');
 const fetch = require('node-fetch');
+const mime = require('mime');
+const path = require("path");
 
 var s3 = new AWS.S3({
     apiVersion: '2006-03-01',
@@ -14,16 +16,20 @@ var s3 = new AWS.S3({
 });
 
 const bucket_name = "test1";
-const object_name = "5.txt";
+const file_path = '/root/1.jpg';
+
+const object_name = path.basename(file_path);
 
 const params = {
     Expires: 30,
     Bucket: bucket_name,
     Key: object_name,
-    Conditions: [["content-length-range", 1, 10000000]], // you can upload file size range from 1Byte to 10MB
-    Fields: {
-        "key": object_name
-    }
+    Conditions: [
+        ["content-length-range", 0, 10000000],  // you can upload file size range from 1Byte to 10MB
+        // ["eq", "$Content-Type", "image/jpeg"],
+        ["starts-with", "$Content-Type", ""],
+        ["starts-with", "$key", ""],
+    ],
 };
 
 s3.createPresignedPost(params, (err, data) => {
@@ -36,7 +42,11 @@ s3.createPresignedPost(params, (err, data) => {
         for(const field in data.fields) {
             form.append(field, data.fields[field]);
         }
-        form.append('file', fs.createReadStream("/root/1.txt"));
+
+        form.append('key', object_name);
+        form.append('Content-Type', mime.getType(file_path));
+        form.append('file', fs.createReadStream(file_path));
+
         form.getLength((err, length) => {
             console.log(`Length: ${length}`);
             fetch(data.url, {
@@ -52,7 +62,6 @@ s3.createPresignedPost(params, (err, data) => {
                 return response.text();
             }).then((payload) => {
                 console.log(payload);
-                console.log(form.getHeaders());
             }).catch((err) => {
                 console.log(`Error: ${err}`)
             });
